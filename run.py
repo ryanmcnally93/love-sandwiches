e6d44e5ee5afd0086c8933080d5edda883a090c0
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-06-12 10:03:30 +0000

    obtain 5 last entries from each item in sales

--- run.py
+++ run.py
@@ -101,6 +101,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet,
+    returns data to a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -113,4 +127,4 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+sales_columns = get_last_5_entries_sales()
